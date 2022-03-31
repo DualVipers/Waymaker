3c92889ba6a95965767fd51eee1894d94db34240
JRed <joseph@titaniumcoast.com>
2022-03-31 00:02:22 -0400

    Added Exports For Matchers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 const Wayfinder = require("./Wayfinder");
 
 module.exports = Wayfinder;
+
+const DomainMatcher = require("./matchers/domain");
+const SubdomainMatcher = require("./matchers/subdomain");
+
+module.exports.matchers = {
+    DomainMatcher,
+    SubdomainMatcher,
+};
